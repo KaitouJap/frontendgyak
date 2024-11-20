f6b9a621bb8719228e34e993ca8357150f084409
KaitouJap <kaitoujap@gmail.com>
2024-11-20 15:38:38 +0100

    todolist 1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import {useState} from 'react'
+import './todolist.css'
 
+type Task = {
+  name: string;
+  completed: boolean;
+}
 
 function App() {
-  const [tasks, setTasks] = useState<string[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [newTask, setNewTask] = useState('')
   const addTask = () => {
     if(newTask.trim() === '') return;
-    setTasks((prevTasks) => [...prevTasks, newTask]);
+    setTasks((prevTasks) => [...prevTasks, {name: newTask, completed: false}]);
     setNewTask('');
   };
   const removeTask = (indexToRemove: number) => {
     setTasks((prevTask) => prevTask.filter((_, index) => index !== indexToRemove));
+  };
+  const toggleTaskCompleted = (index: number) => {
+    setTasks((prevTask) => 
+      prevTask.map((task, i) =>
+      i === index ? {...task, completed: !task.completed} : task));
   };
   return(
     <div>
@@ -20,8 +30,9 @@ function App() {
       <ul>
         {tasks.map((task, index) => (
           <li key={index}>
-            {task}
-            <button onClick={()=>removeTask(index)}>Torles</button>
+            <span style={task.completed?{textDecoration: "line-through"}:{}}>{task.name}</span>
+            <button onClick={()=>removeTask(index)}>torles</button>
+            <button onClick={()=>toggleTaskCompleted(index)}>{task.completed?"Visszaalittas":"Kesz"}</button>
           </li>
         ))}
       </ul> 
